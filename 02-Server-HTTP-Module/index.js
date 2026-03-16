3ed109290644fb671da0f60b9bc6842620554123
Gabriel7721 <116139790+Gabriel7721@users.noreply.github.com>
2026-03-16 20:16:47 +0700

    update

--- 02-Server-HTTP-Module/index.js
+++ 02-Server-HTTP-Module/index.js
@@ -1,6 +1,11 @@
 const http = require("http");
 
-let users = [];
+let users = [
+  {
+    id: 1,
+    name: "Tom",
+  },
+];
 let currentId = 1;
 
 const server = http.createServer((request, response) => {
@@ -27,15 +32,29 @@ const server = http.createServer((request, response) => {
 
     request.on("end", () => {
       const data = JSON.parse(body);
+      const newUser = {
+        id: currentId++,
+        name: data.name,
+      };
+      users.push(newUser);
+      response.end(JSON.stringify(newUser));
+    });
+  } else if (url.startsWith("/users/") && method === "GET") {
+    /**
+     * Method 03: GET | endpoint: "/users/:id"
+     * Truy vấn array lấy ra user theo id tương ứng
+     * "http://localhost:9999/users/2"
+     * ["localhost:9999", "users", "2"]
+     * [0, 1, 2]
+     * "2" => parseInt => 2
+     */
+    const id = parseInt(url.split("/")[2]);
+
+    const user = users.find((u) => {
+      u.id === id;
     });
 
-    const newUser = {
-      id: currentId++,
-      name: data.name,
-    };
-
-    users.push(newUser);
-    response.end(JSON.stringify(newUser));
+    response.end(JSON.stringify(user));
   }
 });
 
